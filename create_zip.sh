4a546db1fd1f30f7fe9461e1910c3d28aad13a26
Timothy Jewell <timothydjewell@gmail.com>
2018-09-27 01:39:14 -0700

    add debug code to create_zip.sh

--- create_zip.sh
+++ create_zip.sh
@@ -4,6 +4,12 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cd VSCode-darwin
     zip -r ../VSCode-darwin-${LATEST_MS_TAG}.zip ./*
+  elif [[ "$APPVEYOR_OS_NAME" == "windows" ]]; then
+    echo "Available directories:"
+    ls -d */
+    cd VSCode-win32-x64
+    echo "Directory contents"
+    ls
   else
     cd VSCode-linux-x64
     tar czf ../VSCode-linux-x64-${LATEST_MS_TAG}.tar.gz .
