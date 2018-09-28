31dba8040c91fdcdf77ea64aaf9f84970373d5e0
Timothy Jewell <timothydjewell@gmail.com>
2018-09-28 01:46:55 -0700

    Try to zip using 7z

--- create_zip.sh
+++ create_zip.sh
@@ -5,11 +5,8 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
     cd VSCode-darwin
     zip -r ../VSCode-darwin-${LATEST_MS_TAG}.zip ./*
   elif [[ "$CI_WINDOWS" == "True" ]]; then
-    echo "Available directories:"
-    ls -d */
     cd VSCode-win32-x64
-    echo "Directory contents"
-    ls
+    7z a ../VSCode-win32-x64-${LATEST_MS_TAG}.zip ./*
   else
     cd VSCode-linux-x64
     tar czf ../VSCode-linux-x64-${LATEST_MS_TAG}.tar.gz .
