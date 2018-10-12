7c0029cf53b211be6ae52a6e47e0ec339bacb8b4
Timothy Jewell <timothydjewell@gmail.com>
2018-10-12 09:17:30 -0700

    fixup! debug package location

--- build.ps1
+++ build.ps1
@@ -15,11 +15,20 @@ bash ../customize_product_json.sh
 bash ../undo_telemetry.sh
 
 Write-Output "APPDATA: '$env:APPDATA'"
+Write-Output "YARN GLOBAL BIN $(yarn global bin)"
+Write-Output "YARN GLOBAL DIR $(yarn global dir)"
 Write-Output "NPM ROOT: $(npm root -g)"
 Write-Output "Recording NPM locations:"
 Get-ChildItem "$env:APPDATA\npm" | Write-Output
 Write-Output "Recording other possible NPM locations:"
 Get-ChildItem "$env:APPDATA\npm\node_modules" | Write-Output
+Write-Output "Recording npm root"
+Get-ChildItem "$(npm root -g)" | Write-Output
+
+Write-Output "Recording yarn bin location"
+Get-ChildItem "$(yarn global bin)" | Write-Output
+Write-Output "Recording yarn dir location"
+Get-ChildItem "$(yarn global dir)" | Write-Output
 
 npm run gulp "vscode-win32-$env:BUILDARCH-min"
 npm run gulp "vscode-win32-$env:BUILDARCH-copy-inno-updater"
